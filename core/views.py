f3a1b3ef6e21e6e4fc87d0414fff1f2d84a4d3fa
carlostorresrj <carlotorresrj@gmail.com>
2021-06-07 19:17:59 -0400

    Commit da primeira aplicação Django, Retornando Hello World

---
+++ core/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def hello(requests, nome, idade):
+    return HttpResponse('<h1>Hello {} de {} anos</h1>'.format(nome, idade))
